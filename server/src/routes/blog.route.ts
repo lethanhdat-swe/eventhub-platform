cb63104c4a8dca04b620572f7b375f72af677688
Nguyen Vo Gia Bao <nbao02006.tgdd@gmail.com>
2026-05-09 19:58:02 +0700

    feat:category+blog

---
+++ server/src/routes/blog.route.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import blogController from "../controllers/blog.controller";
+import { validate } from "../middlewares/validate.middleware";
+import { isAuth, restrictTo } from "../middlewares/auth.middleware";
+import {
+    createBlogSchema,
+    deleteBlogSchema,
+    getBlogSchema,
+    listBlogSchema,
+    updateBlogSchema,
+} from "../schema/blog.schema";
+
+const router = Router();
+
+router.get("/", validate(listBlogSchema), blogController.getAll);
+router.get("/:id", validate(getBlogSchema), blogController.getOne);
+
+// Admin only routes
+router.use(isAuth, restrictTo("admin"));
+
+router.post("/", validate(createBlogSchema), blogController.create);
+router.patch("/:id", validate(updateBlogSchema), blogController.update);
+router.delete("/", validate(deleteBlogSchema), blogController.delete);
+
+export default router;
