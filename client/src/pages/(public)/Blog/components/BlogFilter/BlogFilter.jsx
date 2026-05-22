3cbade6daf4d7ca98a9ea13f927e6d46e4312a43
Nguyen Vo Gia Bao <nbao02006.tgdd@gmail.com>
2026-05-22 21:09:56 +0700

    done render category-blog-filter+contact

--- client/src/pages/(public)/Blog/components/BlogFilter/BlogFilter.jsx
+++ client/src/pages/(public)/Blog/components/BlogFilter/BlogFilter.jsx
@@ -1,19 +1,31 @@
-import { blogData } from "../../data";
+import { blogData } from '../../data';
 
-function BlogFilter() {
-    return (
-        <div className="bg-(--background-color)/90 border border-(--text-primary)/10 rounded-4xl mt-5 p-5 container">
-            <div className="flex items-center justify-around">
-                {blogData.categories.map((category) => (
-                <p key={category.label} className={`px-4 py-2 rounded-2xl flex items-center justify-center text-xl
-              transition-all duration-300
-                    ${category.active ? 'text-(--text-primary) scale-105 shadow-lg shadow-purple-500/30 bg-(--primary-color)'
-                            : 'bg-(--background-color)/70 border border-(--text-primary)/10 text-gray-400 hover:bg-(--text-primary)/10 hover:text-(--text-primary) hover:scale-105'}
-                    `}>{category.label}</p>
-            ))}
-            </div>
-        </div>
-     );
+function BlogFilter({ selectedCategory, onSelectCategory }) {
+  return (
+    <div className="bg-(--background-color)/90 border border-(--text-primary)/10 rounded-4xl mt-5 p-5 container">
+      <div className="flex items-center justify-around flex-wrap gap-3">
+        {blogData.categories.map((category) => {
+          const isActive = category.label === selectedCategory;
+          return (
+            <button
+              key={category.label}
+              onClick={() => onSelectCategory(category.label)}
+              className={`px-4 py-2 rounded-2xl flex items-center justify-center text-xl
+                            transition-all duration-300 cursor-pointer border-0 outline-hidden
+                            ${
+                              isActive
+                                ? 'text-(--text-primary) scale-105 shadow-lg shadow-purple-500/30 bg-(--primary-color)'
+                                : 'bg-(--background-color)/70 border border-(--text-primary)/10 text-gray-400 hover:bg-(--text-primary)/10 hover:text-(--text-primary) hover:scale-105'
+                            }
+                            `}
+            >
+              {category.label}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default BlogFilter;
